6d1891fddd828aeda43a0eb075158fb5c13bb604
Bernhard Schussek <bschussek@gmail.com>
2014-12-05 12:41:31 +0100

    Fixed file doc comments

--- tests/Package/PackageFile/PackageFileTest.php
+++ tests/Package/PackageFile/PackageFileTest.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * This file is part of the Puli Repository Manager package.
+ * This file is part of the puli/repository-manager package.
  *
  * (c) Bernhard Schussek <bschussek@gmail.com>
  *
