2b9f85b49558cb18aaa67c0a13ec9b8745d04f38
Bernhard Schussek <bschussek@gmail.com>
2015-01-04 13:38:19 +0100

    Added DistinguishedName

---
+++ src/Util/DistinguishedName.php
@@ -0,0 +1,182 @@
+<?php
+
+/*
+ * This file is part of the puli/repository-manager package.
+ *
+ * (c) Bernhard Schussek <bschussek@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Puli\RepositoryManager\Util;
+
+use OutOfBoundsException;
+use Puli\Repository\Assert\Assertion;
+
+/**
+ * A LDAPv3 Distinguished Name.
+ *
+ * @since  1.0
+ * @author Bernhard Schussek <bschussek@gmail.com>
+ * @link   https://www.ietf.org/rfc/rfc2253.txt
+ */
+class DistinguishedName
+{
+    /**
+     * @var array
+     */
+    private $attributes = array();
+
+    /**
+     * Creates a distinguished name.
+     *
+     * @param array $attributes The attribute values indexed by the attribute
+     *                          names.
+     */
+    public function __construct(array $attributes = array())
+    {
+        $this->merge($attributes);
+    }
+
+    /**
+     * Adds an attribute to the name.
+     *
+     * @param string $name  The attribute name. Must start with a letter and
+     *                      contain letters, digits and hyphens only.
+     * @param string $value The attribute value. Any non-empty string is
+     *                      allowed.
+     *
+     * @see merge()
+     */
+    public function add($name, $value)
+    {
+        Assertion::string($name, 'The attribute name must be a string. Got: %2$s');
+        Assertion::notEmpty($name, 'The attribute name must not be empty.');
+        Assertion::true((bool) preg_match('~^[a-zA-Z][a-zA-Z0-9\-]*$~', $name), sprintf(
+            'The attribute name must start with a letter and consist of '.
+            'letters, numbers and hyphens only. Got: "%s"',
+            $name
+        ));
+        Assertion::string($value, 'The attribute value must be a string. Got: %2$s');
+
+        $this->attributes[$name] = $value;
+    }
+
+    /**
+     * Adds multiple attributes to the name.
+     *
+     * @param array $attributes The attributes to add.
+     *
+     * @see add()
+     */
+    public function merge(array $attributes)
+    {
+        foreach ($attributes as $name => $value) {
+            $this->add($name, $value);
+        }
+    }
+
+    /**
+     * Removes an attribute.
+     *
+     * @param string $name The attribute name.
+     *
+     * @see add()
+     */
+    public function remove($name)
+    {
+        unset($this->attributes[$name]);
+    }
+
+    /**
+     * Returns the value of an attribute.
+     *
+     * @param string $name The attribute name.
+     *
+     * @return string The attribute value.
+     *
+     * @see add(), has()
+     */
+    public function get($name)
+    {
+        if (!isset($this->attributes[$name])) {
+            throw new OutOfBoundsException(sprintf(
+                'The attribute "%s" was not defined.',
+                $name
+            ));
+        }
+
+        return $this->attributes[$name];
+    }
+
+    /**
+     * Returns whether an attribute is set.
+     *
+     * @param string $name The attribute name.
+     *
+     * @return bool Whether the attribute was set.
+     *
+     * @see add(), get()
+     */
+    public function has($name)
+    {
+        return isset($this->attributes[$name]);
+    }
+
+    /**
+     * Returns the attributes as array.
+     *
+     * @return string[] The attribute values indexed by the attribute names.
+     */
+    public function toArray()
+    {
+        return $this->attributes;
+    }
+
+    /**
+     * Returns the distinguished name as string.
+     *
+     * Attribute values are always enquoted with '"'. Quotation marks and
+     * backslashes are escaped with a prefix backslash in the values.
+     *
+     * @return string The string form of the distinguished name.
+     */
+    public function toString()
+    {
+        $stringAttr = array();
+
+        foreach ($this->attributes as $name => $value) {
+            $stringAttr[] = $name.'='.$this->escape($value);
+        }
+
+        return implode(',', $stringAttr);
+    }
+
+    /**
+     * @see toString()
+     */
+    public function __toString()
+    {
+        return $this->toString();
+    }
+
+    private function escape($value)
+    {
+        // see https://www.ietf.org/rfc/rfc2253.txt
+
+        // We quote the string with '"', thus we can ignore special characters.
+        // From the RFC:
+
+        // string     = *( stringchar / pair )
+        //              / "#" hexstring
+        //              / QUOTATION *( quotechar / pair ) QUOTATION ; only from v2
+        // quotechar  = <any character except "\" or QUOTATION >
+        // QUOTATION  =  <the ASCII double quotation mark character '"' decimal 34>
+
+        return '"'.preg_replace_callback('~[\\\\"]~', function ($matches) {
+            return '\\'.$matches[0];
+        }, $value).'"';
+    }
+
+}
---
+++ tests/Util/DistinguishedNameTest.php
@@ -0,0 +1,177 @@
+<?php
+
+/*
+ * This file is part of the puli/repository-manager package.
+ *
+ * (c) Bernhard Schussek <bschussek@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Puli\RepositoryManager\Tests\Util;
+
+use PHPUnit_Framework_TestCase;
+use Puli\RepositoryManager\Util\DistinguishedName;
+use stdClass;
+
+/**
+ * @since  1.0
+ * @author Bernhard Schussek <bschussek@gmail.com>
+ */
+class DistinguishedNameTest extends PHPUnit_Framework_TestCase
+{
+    public function testAdd()
+    {
+        $dn = new DistinguishedName();
+        $dn->add('cn', 'Manager');
+        $dn->add('ou', 'Sales');
+
+        $this->assertSame('Manager', $dn->get('cn'));
+        $this->assertSame('Sales', $dn->get('ou'));
+        $this->assertSame(array('cn' => 'Manager', 'ou' => 'Sales'), $dn->toArray());
+    }
+
+    public function getValidNames()
+    {
+        return array(
+            array('cn1'),
+            array('c-n'),
+        );
+    }
+
+    /**
+     * @dataProvider getValidNames
+     */
+    public function testAddAcceptsValidNames($name)
+    {
+        $dn = new DistinguishedName();
+        $dn->add($name, 'Manager');
+
+        $this->assertSame('Manager', $dn->get($name));
+    }
+
+    public function getInvalidNames()
+    {
+        return array(
+            array('ćn'),
+            array('c,n'),
+            array('1cn'),
+            array('cö'),
+            array('c='),
+        );
+    }
+
+    /**
+     * @dataProvider getInvalidNames
+     * @expectedException \InvalidArgumentException
+     */
+    public function testAddRejectsInvalidNames($name)
+    {
+        $dn = new DistinguishedName();
+        $dn->add($name, 'Manager');
+    }
+
+    public function getInvalidValues()
+    {
+        return array(
+            array(1234),
+            array(true),
+            array(null),
+            array(new stdClass()),
+        );
+    }
+
+    /**
+     * @dataProvider getInvalidValues
+     * @expectedException \InvalidArgumentException
+     */
+    public function testAddRejectsInvalidValues($value)
+    {
+        $dn = new DistinguishedName();
+        $dn->add('cn', $value);
+    }
+
+    public function testMerge()
+    {
+        $dn = new DistinguishedName();
+        $dn->add('cn', 'Manager');
+        $dn->merge(array('ou' => 'Sales', 'o' => 'Company'));
+
+        $this->assertSame('Manager', $dn->get('cn'));
+        $this->assertSame('Sales', $dn->get('ou'));
+        $this->assertSame('Company', $dn->get('o'));
+        $this->assertSame(array('cn' => 'Manager', 'ou' => 'Sales', 'o' => 'Company'), $dn->toArray());
+    }
+
+    public function testCreateWithAttributes()
+    {
+        $dn = new DistinguishedName(array('cn' => 'Manager', 'ou' => 'Sales'));
+
+        $this->assertSame('Manager', $dn->get('cn'));
+        $this->assertSame('Sales', $dn->get('ou'));
+        $this->assertSame(array('cn' => 'Manager', 'ou' => 'Sales'), $dn->toArray());
+    }
+
+    public function testRemove()
+    {
+        $dn = new DistinguishedName();
+        $dn->merge(array('cn' => 'Manager', 'ou' => 'Sales', 'o' => 'Company'));
+        $dn->remove('o');
+
+        $this->assertSame('Manager', $dn->get('cn'));
+        $this->assertSame('Sales', $dn->get('ou'));
+        $this->assertSame(array('cn' => 'Manager', 'ou' => 'Sales'), $dn->toArray());
+    }
+
+    public function testHas()
+    {
+        $dn = new DistinguishedName();
+
+        $this->assertFalse($dn->has('cn'));
+
+        $dn->add('cn', 'Manager');
+
+        $this->assertTrue($dn->has('cn'));
+    }
+
+    /**
+     * @expectedException \OutOfBoundsException
+     */
+    public function testGetFailsIfUnknownName()
+    {
+        $dn = new DistinguishedName();
+        $dn->get('cn');
+    }
+
+    public function testToString()
+    {
+        $dn = new DistinguishedName();
+        $dn->add('cn', 'Manager');
+        $dn->add('ou', 'Sales');
+        $dn->add('o', 'Company');
+
+        $this->assertSame('cn="Manager",ou="Sales",o="Company"', $dn->toString());
+    }
+
+    public function getEscapedValues()
+    {
+        return array(
+            array('cn', ' Manager', 'cn=" Manager"'),
+            array('cn', 'Mana\\ger', 'cn="Mana\\\\ger"'),
+            array('cn', '"Manager"', 'cn="\\"Manager\\""'),
+            array('cn', '', 'cn=""'),
+        );
+    }
+
+    /**
+     * @dataProvider getEscapedValues
+     */
+    public function testToStringEscapesProperly($name, $value, $string)
+    {
+        $dn = new DistinguishedName();
+        $dn->add($name, $value);
+
+        $this->assertSame($string, $dn->toString());
+    }
+}
