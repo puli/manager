a5a3b49ff83e608ead7c1dc8a8a13e0e3a999080
Bernhard Schussek <bschussek@gmail.com>
2015-01-08 11:08:36 +0100

    Removed superfluous linebreak from GeneratedPuliFactory

--- res/template/GeneratedPuliFactory.tpl.php
+++ res/template/GeneratedPuliFactory.tpl.php
@@ -23,7 +23,7 @@ use <?php echo $import ?>;
  *
  * Otherwise any modifications will be overwritten!
  */
-class <?php echo $shortClassName."\n" ?> implements PuliFactory
+class <?php echo $shortClassName ?> implements PuliFactory
 {
     /**
      * Creates the resource repository.
